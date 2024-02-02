05f79e877a5748548c155fac7ca583ca82ea9b88
brian d foy <briandfoy@pobox.com>
2024-02-02 14:42:20 -0500

    Update email address

--- lib/Surveyor/App.pm
+++ lib/Surveyor/App.pm
@@ -153,11 +153,11 @@ it, just ask and we'll work something out.
 
 =head1 AUTHOR
 
-brian d foy, C<< <bdfoy@cpan.org> >>
+brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2013-2023, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2013-2023, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
